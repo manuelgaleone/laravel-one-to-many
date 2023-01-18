f6157ddebf3ceb43ac76c887848fe20868df05b4
manuelgaleone <galeonemanuel14@gmail.com>
2023-01-18 20:39:12 +0100

    Finished migrations, added seeder

---
+++ database/seeders/CategorySeeder.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Category;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Str;
+
+class CategorySeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $categories = ['Laravel', 'Php', 'Graphic Design', 'Figma', 'Vue Js', 'React', 'Front-End', 'Back-End', 'Full-Stack'];
+
+        foreach ($categories as $category) {
+            $new_category = new Category();
+            $new_category->name = $category;
+            $new_category->slug = Str::slug($new_category->name);
+            $new_category->save();
+        }
+    }
+}
